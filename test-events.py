433a0922f570454b8ce65f203a37521dea7f6205
Simon Wright <sjwright@gmail.com>
2025-02-27 00:07:57 +1100

    Neater event log test code

--- test-events.py
+++ test-events.py
@@ -4,31 +4,31 @@ import yaml
 import time
 
 config = yaml.safe_load(open("test-config.yaml"))
-tpi = ZenProtocol(narration=True, unicast=False)
+tpi = ZenProtocol(narration=False, unicast=False)
 ctrl = ZenController(protocol=tpi, **config.get('zencontrol')[0])
 tpi.set_controllers([ctrl])
 
 # Handlers
 def button_press_event(instance: ZenInstance, payload: bytes) -> None:
-    print(f"Button Press Event - address {instance.address.number} instance {instance.number}")
+    print(f"Button Press Event       - ECD {instance.address.number} instance {instance.number}")
 def button_hold_event(instance: ZenInstance, payload: bytes) -> None:
-    print(f"Button Hold Event - address {instance.address.number} instance {instance.number}")
+    print(f"Button Hold Event        - ECD {instance.address.number} instance {instance.number}")
 def absolute_input_event(instance: ZenInstance, payload: bytes) -> None:
-    print(f"Absolute Input Event - address {instance.address.number} instance {instance.number}")
-def level_change_event(address: ZenAddress, arc_level: int, payload: bytes) -> None:
-    print(f"Level Change Event - address {address.number} arc_level {arc_level}")
-def group_level_change_event(address: ZenAddress, arc_level: int, payload: bytes) -> None:
-    print(f"Group Level Change Event - address {address.number} arc_level {arc_level}")
-def scene_change_event(address: ZenAddress, scene: int, payload: bytes) -> None:
-    print(f"Scene Change Event - address {address.number} scene {scene}")
+    print(f"Absolute Input Event     - ECD {instance.address.number} instance {instance.number}")
 def is_occupied_event(instance: ZenInstance, payload: bytes) -> None:
-    print(f"Is Occupied Event - address {instance.address.number} instance {instance.number}")
+    print(f"Is Occupied Event        - ECD {instance.address.number} instance {instance.number}")
+def level_change_event(address: ZenAddress, arc_level: int, payload: bytes) -> None:
+    print(f"Level Change Event       - {address.type} {address.number} arc_level {arc_level}")
+def group_level_change_event(address: ZenAddress, arc_level: int, payload: bytes) -> None:
+    print(f"Group Level Change Event - {address.type} {address.number} arc_level {arc_level}")
+def scene_change_event(address: ZenAddress, scene: int, payload: bytes) -> None:
+    print(f"Scene Change Event       - {address.type} {address.number} scene {scene}")
 def colour_change_event(address: ZenAddress, colour: bytes, payload: bytes) -> None:
-    print(f"Colour Change Event - address {address.number} colour {colour}")
+    print(f"Colour Change Event      - {address.type} {address.number} colour {colour}")
 def profile_change_event(controller: ZenController, profile: int, payload: bytes) -> None:
-    print(f"Profile Change Event - controller {controller.name} profile {profile}")
+    print(f"Profile Change Event     - controller {controller.name} profile {profile}")
 def system_variable_change_event(controller: ZenController, target: int, value: int, payload: bytes) -> None:
-    print(f"System Variable Change Event - controller {controller.name} target {target} value {value}")
+    print(f"System Variable Change   - controller {controller.name} target {target} value {value}")
 
 # Start event monitoring
 tpi.set_callbacks(
