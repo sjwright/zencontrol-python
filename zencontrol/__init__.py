d3352a582a4b5fadbc6f90d112dc946607cf09cf
Simon Wright <sjwright@gmail.com>
2026-07-23 12:46:29 +1000

    Bump version to 0.1.3

--- zencontrol/__init__.py
+++ zencontrol/__init__.py
@@ -58,7 +58,7 @@ from .exceptions import ZenError, ZenTimeoutError, ZenResponseError, ZenConnecti
 # Utilities
 from .utils import run_with_keyboard_interrupt
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Simon Wright"
 
 # Public API - these are the main classes users should import
